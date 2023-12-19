49dce6e3ddf627c884bf93f4089e57df6d9b9de1
Timmy Jose <zoltan.jose@gmail.com>
2023-12-19 12:42:41 +0530

    Working version

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react'
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native'
+import { execute } from 'react-native-bridge-provider'
 
-export default function App() {
+const App = () => {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const handleAdd = () => {
+    const add_cmd = JSON.stringify({
+      Add: {
+        x: x,
+        y: y,
+      }
+    })
+
+    const sum = JSON.parse(execute(add_cmd))
+    setResult(sum.res)
+  }
+
+  const handleSub = () => {
+    const sub_cmd = JSON.stringify({
+      Sub: {
+        x: x,
+        y: y
+      }
+    })
+    const diff = JSON.parse(execute(sub_cmd))
+    setResult(diff.res)
+  }
+
+  const handleMul = () => {
+    const mul_cmd = JSON.stringify({
+      Mul: {
+        x: x,
+        y: y
+      }
+    })
+
+    const prod = JSON.parse(execute(mul_cmd))
+    setResult(prod.res)
+  }
+
+  const handleDiv = () => {
+    const div_cmd = JSON.stringify({
+      Div: {
+        x: x,
+        y: y
+      }
+    })
+
+    const quot = JSON.parse(execute(div_cmd))
+    setResult(quot.res)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 1"
+        keyboardType="numeric"
+        value={x.toString()}
+        onChangeText={(text: string) => setX(parseFloat(text))}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter number 2"
+        keyboardType="numeric"
+        value={y.toString()}
+        onChangeText={(text: string) => setY(parseFloat(text))}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button title="Add" onPress={handleAdd} />
+        <Button title="Subtract" onPress={handleSub} />
+        <Button title="Multiply" onPress={handleMul} />
+        <Button title="Divide" onPress={handleDiv} />
+      </View>
+
+      <Text style={styles.resultText}>Result: {result}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  resultText: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '80%',
+  },
+})
+
+export default App
--- notes.md
+++ notes.md
@@ -4,4 +4,16 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer/ possibly requir
 
 expo prebuild:
 
+    * npx expo prebuild --clean (to be used only when needed)
+    * npx expo prebuild
+    * create script to automate updating config for android and iOS.
+
     Add android and ios directories to .gitignore (https://docs.expo.dev/guides/local-app-development/)
+
+Running:
+
+    yarn install
+    npx expo run:android
+    npx expo run:ios
+
+    npx expo install expo-dev-client (start with `npx expo start --dev-client`)
